7a34c3d50937f829667a6ce6bd96b0cb6688f77a
herakarmila19 <36470298+herakarmila19@users.noreply.github.com>
2026-04-27 13:35:40 +0700

    Fix root redirect to honor deployed module path

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
-// Redirect to public folder
-header('Location: public/');
+
+$uriPath = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH) ?? '/';
+$scriptName = $_SERVER['SCRIPT_NAME'] ?? '';
+
+if ($scriptName !== '' && str_ends_with($uriPath, $scriptName)) {
+    $uriPath = substr($uriPath, 0, -strlen($scriptName));
+}
+
+$basePath = rtrim($uriPath, '/');
+$redirectPath = ($basePath === '' ? '' : $basePath) . '/public/';
+
+header('Location: ' . $redirectPath, true, 302);
 exit;
