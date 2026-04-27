3cb8174e5bc8852dc60687433f838a45c70db8a1
herakarmila19 <36470298+herakarmila19@users.noreply.github.com>
2026-04-27 13:47:30 +0700

    Make routing/baseURL work for both localhost and production paths

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://selatan.jakarta.go.id/modul/sistem-barang/';
+    public string $baseURL = '';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
